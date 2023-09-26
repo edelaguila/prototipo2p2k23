abfb4086da60c9539a30efa4535714b803b9ecb9
GustavoAZepeda <gzepedah2@miumg.edu.gt>
2023-09-26 08:46:39 -0600

    intefaz creada

---
+++ Codigo/MVC/ProyectoMVC/CapaModelo/Sentencias.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Odbc;
+
+namespace CapaModelo
+{
+    public class Sentencias
+    {
+        Conexion cn = new Conexion();
+
+        public OdbcDataAdapter llenarTbl(string tabla)// metodo  que obtinene el contenio de una tabla
+        {
+            //string para almacenar los campos de OBTENERCAMPOS y utilizar el 1ro
+            string sql = "SELECT * FROM " + tabla + "  ;";
+            OdbcDataAdapter dataTable = new OdbcDataAdapter(sql, cn.conexion());
+            return dataTable;
+        }
+
+    }
+}
---
+++ Codigo/MVC/ProyectoMVC/CapaVista/Consulta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaControlador;
+
+namespace CapaVista
+{
+    public partial class Consulta : Form
+    {
+        string emp = "empleados";
+        Controlador cn = new Controlador();
+
+        public void actualizardatagriew()
+        {
+            DataTable dt = cn.llenarTbl(emp);
+           dataGridView1.DataSource = dt;
+
+        }
+
+
+        public Consulta()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            actualizardatagriew();
+        }
+    }
+}
