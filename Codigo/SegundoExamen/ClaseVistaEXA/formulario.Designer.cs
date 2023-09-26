2c6e42434b4700aa204424ff572b82d663017aee
JustinPennant <jramosp14@miumg.edu.gt>
2023-09-26 09:29:08 -0600

    Trabajado en Segundo Examen

---
+++ Codigo/SegundoExamen/ClaseVistaEXA/formulario.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace ClaseVistaEXA
+{
+    partial class formulario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.combo1 = new CapaVista.Combo();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // combo1
+            // 
+            this.combo1.Location = new System.Drawing.Point(96, 150);
+            this.combo1.Name = "combo1";
+            this.combo1.Size = new System.Drawing.Size(271, 29);
+            this.combo1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Justin Emmanuel Ramos Pennant";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "0901-20-1538";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(96, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tabla: cursos";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(471, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Cargar Datos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(471, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(184, 31);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Ayuda";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(471, 130);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(184, 31);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Reporte";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(471, 176);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(184, 31);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Salir";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // formulario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.combo1);
+            this.Name = "formulario";
+            this.Text = "formulario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CapaVista.Combo combo1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ Codigo/SegundoExamen/ClaseVistaEXA/formulario.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClaseVistaEXA
+{
+    public partial class formulario : Form
+    {
+        public formulario()
+        {
+            InitializeComponent();
+            cargardatos();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //SALIR
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //CARGAR DATOS
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //AYUDA
+            Help.ShowHelp(this, "AyudaRisko/AyudaRiskoAS2.chm", "Venta.html");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //REPORTE
+            Reporte frm = new Reporte();
+            frm.Show();
+        }
+
+        void cargardatos()
+        {
+
+
+
+            combo1.llenarse("codigo_curso", "nombre_curso", "estatus_curso");
+
+
+        }
+
+    }
+}
