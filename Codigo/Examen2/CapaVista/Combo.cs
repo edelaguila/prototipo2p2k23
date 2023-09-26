fae164fd9d06474fe6c19c95082159dcfec81ab9
JonathanArriaga10 <jonathanivan.a.c@gmail.com>
2023-09-26 09:28:00 -0600

    Se agrego el master

---
+++ Codigo/Examen2/CapaVista/Combo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaControlado;
+namespace CapaVista
+{
+    public partial class Combo : UserControl
+    {
+        public Combo()
+        {
+            InitializeComponent();
+        }
+        Controlador cn = new Controlador();
+
+        public void llenarse(string tabla, string campo1, string campo2)
+        {
+
+            string tbl = tabla;
+            string cmp1 = campo1;
+            string cmp2 = campo2;
+          
+
+           
+            comboBox1.ValueMember = "numero";
+            comboBox1.DisplayMember = "nombre";
+
+            string[] items = cn.items(tabla, campo1, campo2);
+
+
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i] != null)
+                {
+                    if (items[i] != "")
+                    {
+                        comboBox1.Items.Add(items[i]);
+                    }
+                }
+
+            }
+
+            var dt2 = cn.enviar(tabla, campo1, campo2);
+            AutoCompleteStringCollection coleccion = new AutoCompleteStringCollection();
+            foreach (DataRow row in dt2.Rows)
+            {
+
+                coleccion.Add(Convert.ToString(row[campo1]) + "-" + Convert.ToString(row[campo2]));
+                coleccion.Add(Convert.ToString(row[campo2]) + "-" + Convert.ToString(row[campo1]));
+
+
+            }
+
+            comboBox1.AutoCompleteCustomSource = coleccion;
+            comboBox1.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            comboBox1.AutoCompleteSource = AutoCompleteSource.CustomSource;
+
+
+        }
+
+
+    }
+}
---
+++ Codigo/ExamenMaster/ExamenMaster/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamenMaster
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string mensaje = "Desea Salir";
+            string titulo = "Salir";
+            MessageBoxButtons boton = MessageBoxButtons.YesNo;
+            DialogResult resultado;
+
+            resultado = MessageBox.Show(mensaje, titulo, boton);
+            if (resultado == System.Windows.Forms.DialogResult.Yes)
+            {
+                // Closes the parent form.
+                this.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            combo1.llenarse("maestros", "codigo_maestro", "nombre_maestro");
+        }
+    }
+}
