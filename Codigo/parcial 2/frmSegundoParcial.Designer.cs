94110ec82a24094de6ff99aac09fd8545f727b10
Jorgejl7 <jorgelopezen7@gmail.com>
2023-09-26 09:24:02 -0600

    subiendo cambios

---
+++ Codigo/parcial 2/frmSegundoParcial.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace parcial_2
+{
+    partial class frmSegundoParcial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_carga_datos = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btn_ayuda = new System.Windows.Forms.Button();
+            this.btn_reporte = new System.Windows.Forms.Button();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(95, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "0901-20-9731";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(85, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(317, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Jorge Elmar Lopez Enriquez";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(95, 279);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(292, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Tabla: Nombre de la tabla";
+            // 
+            // btn_carga_datos
+            // 
+            this.btn_carga_datos.Location = new System.Drawing.Point(551, 61);
+            this.btn_carga_datos.Name = "btn_carga_datos";
+            this.btn_carga_datos.Size = new System.Drawing.Size(179, 50);
+            this.btn_carga_datos.TabIndex = 6;
+            this.btn_carga_datos.Text = "Carga Datos";
+            this.btn_carga_datos.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(100, 336);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(409, 24);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // btn_ayuda
+            // 
+            this.btn_ayuda.Location = new System.Drawing.Point(551, 127);
+            this.btn_ayuda.Name = "btn_ayuda";
+            this.btn_ayuda.Size = new System.Drawing.Size(179, 50);
+            this.btn_ayuda.TabIndex = 8;
+            this.btn_ayuda.Text = "Ayuda";
+            this.btn_ayuda.UseVisualStyleBackColor = true;
+            this.btn_ayuda.Click += new System.EventHandler(this.btn_ayuda_Click);
+            // 
+            // btn_reporte
+            // 
+            this.btn_reporte.Location = new System.Drawing.Point(551, 196);
+            this.btn_reporte.Name = "btn_reporte";
+            this.btn_reporte.Size = new System.Drawing.Size(179, 50);
+            this.btn_reporte.TabIndex = 9;
+            this.btn_reporte.Text = "Reporte";
+            this.btn_reporte.UseVisualStyleBackColor = true;
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Location = new System.Drawing.Point(551, 272);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(179, 50);
+            this.btn_salir.TabIndex = 10;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // frmSegundoParcial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_reporte);
+            this.Controls.Add(this.btn_ayuda);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_carga_datos);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmSegundoParcial";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_carga_datos;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btn_ayuda;
+        private System.Windows.Forms.Button btn_reporte;
+        private System.Windows.Forms.Button btn_salir;
+    }
+}
---
+++ Codigo/parcial 2/frmSegundoParcial.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace parcial_2
+{
+    public partial class frmSegundoParcial : Form
+    {
+        public frmSegundoParcial()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           
+                DialogResult result = MessageBox.Show("¿Estás seguro de que deseas salir del sistema?", "Confirmar Salida", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    Application.Exit(); // Cierra la aplicación.
+                }
+            
+        }
+
+        private void btn_ayuda_Click(object sender, EventArgs e)
+        {
+            string mensajeAyuda = "Explicación de los botones:\n\n" +
+                          "1. Botón 1: Cargar Datos.\n" +
+                          "2. Botón 2: Ayuda.\n" +
+                          "3. Botón 3: Crea un Reporte.\n" +
+                          "4. Botón 4: Salir.";
+
+            MessageBox.Show(mensajeAyuda, "Ayuda", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
